79544f97cc12e70fa7e17c520120b206e862f928
Matt Valle <mattvalle28@gmail.com>
2020-03-09 21:26:04 -0400

    Authentication

--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -1,34 +1,67 @@
+const jwt = require('jsonwebtoken');
+
 const db = require('../models');
 
 exports.register = async (req, res, next) => {
     try {
         const user = await db.User.create(req.body);
-        const { id, username } = user;
+        const {
+            id,
+            username
+        } = user;
 
-        res.json({id, username});
-    } catch(err){
+        const token = jwt.sign({
+            id,
+            username
+        }, process.env.SECRET);
+
+        res.status(201).json({
+            id,
+            username,
+            token
+        });
+
+    } catch (err) {
+        if (err.code === 11000) {
+            err.message = 'Sorry, that username is already taken';
+        }
+
         next(err);
     }
 }
 
 exports.login = async (req, res, next) => {
     try {
-        const user = await db.User.findOne({ username: req.body.username });
-        const {id, username} = user;
+        const user = await db.User.findOne({
+            username: req.body.username
+        });
+        const {
+            id,
+            username
+        } = user;
         const valid = await user.comparePassword(req.body.password);
 
-        if (valid){
-            res.json({
-                id, 
+        if (valid) {
+            const token = jwt.sign({
+                id,
                 username
+            }, process.env.SECRET);
+
+            res.json({
+                id,
+                username,
+                token
             });
         } else {
-            throw new Error('Invalid Username/Password')
+            throw new Error();
         }
 
 
-    } catch(err){
+    } catch (err) {
+        console.log (err)
+        err.message = 'Invalid Username/Password';
+
         next(err);
     }
-        
-    };
+
+};
